b5dd228bba398a79f7310624ef9d185e8d408138
nagygeri97 <nagygeri97@gmail.com>
2020-04-05 23:34:17 +0200

    Template matching templates

--- src/test/TemplateMatchingTests.py
+++ src/test/TemplateMatchingTests.py
@@ -22,10 +22,19 @@ def getTemplateData(templateType):
 	if templateType == TemplateType.PARK:
 		templateFile = "park1.jpg"
 		matchingFiles = ["park" + str(i) + ".jpg" for i in range(2,9)]
-		templatePositions = [] # TODO
+		templatePositions = [( 35,365), # Parking sign (missing in zoomed)
+							 (207,442), # Bin
+							 ( 79,434), # Red car rear right
+							 (226,364), # Tree branches
+							 (120,470), # Pole bottom
+							 (147,263), # Building roof corner
+							 (222,288), # Building window
+							 (332,418), # Park entrance (missing in zoomed)
+							 ( 93,388), # Building bottom
+							]
 	elif templateType == TemplateType.FLAG:
 		templateFile = "flag1.jpg"
-		matchingFiles = ["flag" + str(i) + ".jpg" for i in range(2,5)][:1]
+		matchingFiles = ["flag" + str(i) + ".jpg" for i in range(2,5)]
 		templatePositions = [(400,220), # EU flag
 							 (313,290), # Yellow car
 							 (290, 35), # Building top open window (missing in zoomed)
@@ -39,22 +48,40 @@ def getTemplateData(templateType):
 	elif templateType == TemplateType.STREET:
 		templateFile = "street1.jpg"
 		matchingFiles = ["street" + str(i) + ".jpg" for i in range(2,6)]
-		templatePositions = [] # TODO
+		templatePositions = [( 75,450), # Bike rear
+						     (328,394), # Bin
+							 (193,434), # Dog bin
+							 (210,378), # Fence
+							 (290,364), # Toilet
+							 (295,157), # Building top floor
+							 (134,377), # Parked car
+							 (134,407), # Hedge
+							 (172,269), # Tree top
+							]
 	elif templateType == TemplateType.CARS:
 		templateFile = "cars1.jpg"
 		matchingFiles = ["cars" + str(i) + ".jpg" for i in range(2,4)]
-		templatePositions = [] # TODO
-
+		templatePositions = [(330,365), # White car light
+							 (100,345), # Black car wheel
+							 (144,152), # Balcony
+							 (175,340), # Black car lamp
+							 (360,555), # Manhole cover
+							 (285,128), # Green building window
+							 (135,285), # Yellow building door
+							 (200,100), # Tree top
+							 (384,375), # White car plate
+							]
 	return (path, templateFile, matchingFiles, templatePositions, outPath)
 
 def templateMatchingTest():
-	templateType = TemplateType.FLAG
+	templateType = TemplateType.STREET
 	qzmiClass = QZMI_NoCentroid
 
 	(path, templateFile, matchingFiles, templatePositions, outPath) = getTemplateData(templateType)
-	circleRadius = 20
+	circleRadius = 10
 
 	printImageWithTemplates(path + templateFile, circleRadius, templatePositions, outPath + templateFile[:-4] + "_template.bmp")
+	# return
 	
 	img = getImgFromFileAsRawNpArray(path + templateFile)
 	originalVecs = []
